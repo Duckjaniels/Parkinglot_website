d09ad5573c54c7ca33b96a084fef451800736a98
Maksym Kaczorowski <maksym73@gmail.com>
2022-04-13 12:33:21 +0200

    navbar styling corrections

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,62 @@
-import React, {useState} from 'react';
-import {HiOutlineMenu} from "react-icons/hi";
-import {RiParkingBoxLine} from "react-icons/ri";
-import {Link} from 'react-router-dom';
-import {data} from '../../constants';
+import React, { useState } from "react";
+import { HiOutlineMenu } from "react-icons/hi";
+import { RiParkingBoxLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import { data } from "../../constants";
 import images from "../../constants/images";
-import './Navbar.scss';
+import "./Navbar.scss";
 
 const Navbar = () => {
-    const [toggleMenu, setToggleMenu] = useState(false);
-    return(
-
-  <nav className={"app__navbar"}>
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className={"app__navbar"}>
       <div className={"app__navbar-logo"}>
-          <img src={images.sign24h} alt ="app logo" />
+        <img src={images.sign24h} alt="app logo" />
       </div>
 
       <ul className="app__navbar-links">
-          {data.navList.map(navList => (<li className={navList.className}> <Link to={navList.to}> {navList.name} </Link></li>))}
+        {data.navList.map((navList) => (
+          <li className={navList.className}>
+            <Link to={navList.to}> {navList.name} </Link>
+          </li>
+        ))}
       </ul>
       <div className="app__navbar-button">
-          <a target="_blank"  rel="noreferrer" href="https://goo.gl/maps/6eJri8Ff7SYJHtAS8" className="p__opensans"> Znajdź nas na mapie</a>
-
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://goo.gl/maps/6eJri8Ff7SYJHtAS8"
+          className="p__opensans"
+        >
+          {" "}
+          Znajdź nas na mapie
+        </a>
       </div>
       <div className="app__navbar-smallscreen">
-          <HiOutlineMenu fontSize={27} onClick ={() => setToggleMenu(true)} />
+        <HiOutlineMenu fontSize={27} onClick={() => setToggleMenu(true)} />
 
-          {toggleMenu && (
-              <div className ="app__navbar-smallscreen_overlay flex__center slide-bottom">
-              <RiParkingBoxLine color ="fff" fontSize={27} className="overlay__close" onClick ={() => setToggleMenu(false)} />
-          <ul className="app__navbar-smallscreen-links">
-              {data.navList.map(navList => (<li className={navList.className} onClick ={() => setToggleMenu(false)}> <Link to={navList.to}> {navList.name} </Link></li>))}
-          </ul>
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <RiParkingBoxLine
+              color="fff"
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen-links">
+              {data.navList.map((navList) => (
+                <li
+                  className={navList.className}
+                  onClick={() => setToggleMenu(false)}
+                >
+                  <Link to={navList.to}> {navList.name} </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
-              )}
-      </div>
-  </nav>
-
-);
-}
+    </nav>
+  );
+};
 export default Navbar;
--- src/components/PhotoCarousel/PhotoCarousel.jsx
+++ src/components/PhotoCarousel/PhotoCarousel.jsx
@@ -1,68 +1,68 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import {images} from "../../constants";
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import { images } from "../../constants";
 
 const PhotoCarousel = () => {
-    return (
-        <div className ="header">
-        <Carousel fade={true} pause={false}>
-            <Carousel.Item interval={5000}>
-                <img
-                    className="d-block w-100"
-                    src={images.gallery01}
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3 className="carousel__title">Parking strzeżony 24h</h3>
-                    <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={5000}>
-                <img
-                    className="d-block w-100"
-                    src={images.gallery02}
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                    <h3 className="carousel__title">Parking strzeżony 24h</h3>
-                    <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={5000}>
-                <img
-                    className="d-block w-100"
-                    src={images.gallery03}
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3 className="carousel__title">Parking strzeżony 24h</h3>
-                    <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={5000}>
-                <img
-                    className="d-block w-100"
-                    src={images.gallery04}
-                    alt="Fourth slide"
-                />
-                <Carousel.Caption>
-                    <h3 className="carousel__title">Parking strzeżony 24h</h3>
-                    <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={5000}>
-                <img
-                    className="d-block w-100"
-                    src={images.gallery05}
-                    alt="Fifth slide"
-                />
-                <Carousel.Caption>
-                    <h3 className="carousel__title">Parking strzeżony 24h</h3>
-                    <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-        </div>
-    )
-}
-export {PhotoCarousel};
+  return (
+    <div className="header">
+      <Carousel fade={true} pause={false}>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src={images.gallery01}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3 className="carousel__title">Parking strzeżony 24h</h3>
+            <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src={images.gallery02}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3 className="carousel__title">Parking strzeżony 24h</h3>
+            <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src={images.gallery03}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3 className="carousel__title">Parking strzeżony 24h</h3>
+            <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src={images.gallery04}
+            alt="Fourth slide"
+          />
+          <Carousel.Caption>
+            <h3 className="carousel__title">Parking strzeżony 24h</h3>
+            <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100"
+            src={images.gallery05}
+            alt="Fifth slide"
+          />
+          <Carousel.Caption>
+            <h3 className="carousel__title">Parking strzeżony 24h</h3>
+            <p className="carousel__subtitle">Kraków, Białoruska-Estońska</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+export { PhotoCarousel };
